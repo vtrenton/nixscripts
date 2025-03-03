1b8eb665de69b6270e0ee1a5a258abd7530ab954
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-03-02 22:51:57 -0600

    added generations and cleaned up output
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,7 @@ if [ $(id -u) -ne 0 ]; then
     exit 1
 fi
 
-pushd /etc/nixos/
+pushd /etc/nixos/ >/dev/null
 nix flake update >/dev/null
 nixos-rebuild switch --upgrade >/dev/null
 
@@ -18,4 +18,8 @@ sudo nix-env --delete-generations +${KEEP_GENERATIONS} --profile /nix/var/nix/pr
 # we have marked the last 5 generations with the last command so the GC wont clean it.
 sudo nix-collect-garbage -d --quiet
 
-popd
+# print out current nix generations for tracking
+echo "NixOS Generations:"
+sudo nix-env --list-generations --profile /nix/var/nix/profiles/system
+
+popd >/dev/null
