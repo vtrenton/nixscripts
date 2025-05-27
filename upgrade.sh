be1e62089be584917296e08e2a1556859522548d
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-05-26 21:38:24 -0500

    cleaned up logic in upgrade
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- upgrade.sh
+++ upgrade.sh
@@ -8,14 +8,8 @@ pushd /etc/nixos/ >/dev/null
 nix flake update >/dev/null
 nixos-rebuild switch --upgrade >/dev/null
 
-# Keep only the last 5 generations of the system profile
-KEEP_GENERATIONS=5
-echo "Deleting generations older than the last $KEEP_GENERATIONS..."
-sudo nix-env --delete-generations +${KEEP_GENERATIONS} --profile /nix/var/nix/profiles/system
-
-# Run garbage collection to remove unused files
-# Nix GC is reference aware
-# we have marked the last 5 generations with the last command so the GC wont clean it.
+# collect garbage
+echo "Collecting garbage..."
 sudo nix-collect-garbage -d --quiet
 
 # print out current nix generations for tracking
